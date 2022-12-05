05ac391ecb653134eb0cbb47a15761d6a9dab796
rsweethut <ravichandra1019@gmail.com>
2022-12-05 23:55:36 +0530

    java.lang.IllegalStateException: Unable to find a @SpringBootConfiguration Error Fix

--- src/test/java/com/java2blog/SpringBootJDBCExample/SpringBootJdbcExampleApplicationTests.java
+++ src/test/java/com/java2blog/SpringBootJDBCExample/SpringBootJdbcExampleApplicationTests.java
@@ -6,8 +6,10 @@ import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
+@ContextConfiguration
 class SpringBootJdbcExampleApplicationTests {
 
 	public static Logger LOGGER = LoggerFactory.getLogger(SpringBootJdbcExampleApplicationTests.class);
